b13e8ce639a3259d561d4a74bd40a00f0eeb7e4a
Ivan Fang <iFang.py@gmail.com>
2024-03-03 00:53:21 +1100

    Updated helper function
    express and database setup is now global as it should be

--- backend/server/helper.js
+++ backend/server/helper.js
@@ -1,16 +1,37 @@
+const myDb = require('mariadb');
+const db = mariadb.createPool({
+    host:'192.168.1.107',
+    user: 'user',
+    password: 'arenacatupdatefreeze1!',
+    connectionLimit: 5,
+    acquiteTimeout: 30000
+});
+
+
+const express = require('express');
+const app = express ();
+app.use(express.json)
+
+const PORT = process.env.PORT || 3306;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+
+app.get("/status", (request, response) => {
+    // const status = {
+    //     "Status": "Running"
+    // };
+    let status = updateMedicationStatus();
+
+    response.send(status)
+});
+
 function getDeviceDetails(DeviceID) {
     // get device details from db
     // we select * from Devices d where d.Device_ID = Device_ID;
     // we select * from Patients p join Medicine_History mh on mh.Patient_ID = p.Patient_ID where Device_ID = mh.Device_ID
     // return both device details and patient details
-    const myDb = require('mariadb');
-    const db = mariadb.createPool({
-        host:'192.168.1.107',
-        user: 'user',
-        password: 'arenacatupdatefreeze1!',
-        connectionLimit: 5,
-        acquiteTimeout: 30000
-    });
 
     let deviceInfo;
     let patientInfo;
@@ -50,23 +71,4 @@ function getDeviceDetails(DeviceID) {
 function updateMedicationStatus(DeviceID) {
     // Communicate to specfied device
     // update web ui with flow rate, time remaining and volume remaining
-    const express = require('express');
-    const app = express ();
-    app.use(express.json)
-
-    const PORT = process.env.PORT || 3306;
-
-    app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`)
-    })
-
-    app.get("/status", (request, response) => {
-        const status = {
-            "Status": "Running"
-        };
-
-        response.send(status)
-    });
-
-    // app get the client's info
 }
